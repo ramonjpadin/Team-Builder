aac242b6d9a502f80702ef90cfce829cc64007c2
Ramon Padin <ramonjpadin@gmail.com>
2022-10-26 15:28:53 -0500

    got manager js in

---
+++ test/manager.test.js
@@ -0,0 +1,25 @@
+const Manager = require('../lib/manager');
+
+describe("Manager", () => {
+
+    describe("Initialization", () => {
+        it("Should make an object with entered name, id, and email address and Github", () => {
+            const manager = new Manager("Ramon Padin", 100, "ramonjpadin@gmail.com", 5);
+
+            expect(manager.name).toEqual("Ramon Padin");
+            expect(manager.id).toEqual(100);
+            expect(manager.email).toEqual("ramonjpadin@gmail.com");
+            expect(manager.officeNumber).toEqual(5);
+            expect(manager.role).toEqual("Manager");
+        })
+    })
+});
+
+describe("officeNumber", () => {
+    it("Should record the office number whenever it is entered", () => {
+        const input = 5;
+        const result = new Manager("Ramon Padin", 100, "ramonjpadin@gmail.com", input).getOfficeNumber();
+
+        expect(input).toEqual(result);
+    })
+});
